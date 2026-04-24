c65f29b6b1ee57e282472af258bc8a2f27a80e29
kukhmax <kukhmax@gmail.com>
2026-04-24 12:51:33 +0200

    add new branch bill_bot

--- app/handlers/callbacks.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router, F, types
-
-router = Router()
-
-@router.callback_query(F.data.startswith("open"))
-async def open_trade(callback: types.CallbackQuery):
-    await callback.answer("Автотрейдинг скоро будет 🚀")
--- app/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class SignalSetup(StatesGroup):
-    waiting_pair = State()
-    waiting_tf = State()
-    waiting_adx = State()
-    waiting_atr = State()
-    waiting_risk = State()
